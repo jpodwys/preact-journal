7a5226728a5a9acb40329bfd60d159718a45aeb4
Joseph Podwys <joseph@podwys.com>
2018-01-25 21:04:19 -0700

    no message

--- dist/sw.js
+++ dist/sw.js
@@ -1,17 +1,17 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('preact-journal')
-    .then(cache => cache.addAll(['/index.js']))
+    .then(cache => cache.addAll(['/index.html', '/bundle.js', '/styles.css']))
   )
 })
 
-self.addEventListener('fetch', function(event) {
-  if(event.request.url.indexOf('/api') > -1) return;
-  event.respondWith(
-    caches.match(event.request)
-    .then(response => response
-      ? response
-      : fetch(event.request)
-    )
-  )
-})
+// self.addEventListener('fetch', function(event) {
+//   if(event.request.url.indexOf('/api') > -1) return;
+//   event.respondWith(
+//     caches.match(event.request)
+//     .then(response => response
+//       ? response
+//       : fetch(event.request)
+//     )
+//   )
+// })
