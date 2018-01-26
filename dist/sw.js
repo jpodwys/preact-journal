da07a7fc0c7581b956cba72560dd24301f5c4f0c
Joseph Podwys <joseph@podwys.com>
2018-01-26 10:54:52 -0700

    Remove SW semicolons for now.

--- dist/sw.js
+++ dist/sw.js
@@ -3,19 +3,19 @@ var duplicateEntries = ['/entries', '/entry/new'];
 self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open('preact-journal').then(function(cache) {
-      return cache.addAll(['/', '/bundle.js', '/styles.css']);
+      return cache.addAll(['/', '/bundle.js', '/styles.css'])
     })
-  );
-});
+  )
+})
 
 self.addEventListener('fetch', function(e) {
-  if(e.request.method !== 'GET') return;
-  if(e.request.url.indexOf('/api') > -1) return;
+  if(e.request.method !== 'GET') return
+  if(e.request.url.indexOf('/api') > -1) return
   
   // All routes return the same payload. As such, cache only '/'
   // and return its cached value on all routes.
-  var reqUrl;
-  var url = e.request.url;
+  var reqUrl
+  var url = e.request.url
   if(~url.indexOf('/entries') || ~url.indexOf('/entry/')){
     reqUrl = '/';
   }
