7b25245f045431f70995c282daf0d85eba88a5c6
Joseph Podwys <joseph@podwys.com>
2018-02-16 17:45:06 -0700

    no message

--- assets/js/utils/index.js
+++ assets/js/utils/index.js
@@ -1,5 +1,7 @@
 const findObjectIndexById = function(id, list) {
-  return list.map(function(obj){ return obj.id; }).indexOf(id);
+  return list.map(function(obj){
+    return obj.id;
+  }).indexOf(id);
 };
 
 const removeObjectByIndex = function(index, list) {
