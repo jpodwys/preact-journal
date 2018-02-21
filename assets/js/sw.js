5dcbd69432a50ca8518af1fc535597b5505959c4
Joseph Podwys <joseph@podwys.com>
2018-02-21 12:29:00 -0700

    no message

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -1,47 +1,13 @@
 let version;
-
-// self.addEventListener('install', function(e) {
-//   e.waitUntil(
-//     caches.open('preact-journal').then(function(cache) {
-//       return cache.addAll(['/'])
-//     })
-//   )
-// })
-
-// self.addEventListener('fetch', function(e) {
-//   if(e.request.method !== 'GET') return
-//   if(e.request.url.indexOf('/api') > -1) return
-  
-//   // All routes return the same payload. As such, cache only '/'
-//   // and return its cached value on all routes.
-//   var reqUrl
-//   var url = e.request.url
-//   if(~url.indexOf('/entries') || ~url.indexOf('/entry/')){
-//     reqUrl = '/';
-//   }
-
-//   e.respondWith(
-//     caches.match(reqUrl || e.request)
-//     .then(response => response
-//       ? response
-//       : fetch(e.request)
-//     )
-//   )
-// })
-
-
-
-var CACHE = 'preact-journal';
+const CACHE = 'preact-journal';
  
 self.addEventListener('install', function(e) {
-  console.log('The service worker is being installed.');
   e.waitUntil(caches.open(CACHE).then(function (cache) {
     return cache.addAll(['/'])
   }));
 });
 
 self.addEventListener('fetch', function(e) {
-  console.log('The service worker is serving the asset.');
   if(e.request.method !== 'GET') return
   if(e.request.url.indexOf('/api') > -1) return
   
