c4606330a8ffcc38d64bc4294f14c5c511aa64c7
Joseph Podwys <joseph@podwys.com>
2018-02-22 16:43:45 -0700

    Add empty entry list message.

--- assets/components/entries/index.js
+++ assets/components/entries/index.js
@@ -4,6 +4,11 @@ import EntryPreview from '../entry-preview';
 export default class Entries extends Component {
   render({ entries }) {
     entries = entries || [];
+    if(!entries.length){
+      return (
+        <h2>It's empty in here!</h2>
+      );
+    }
     return (
       <entry-list>
         {entries.map(entry => <EntryPreview entry={entry}/>)}
