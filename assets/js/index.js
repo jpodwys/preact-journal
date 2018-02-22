8fb27e94dc36dcbc3148157b928f9f943348a219
Joseph Podwys <joseph@podwys.com>
2018-02-21 23:18:51 -0700

    Trying another sw fix.

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,3 +2,25 @@ import { h, render } from 'preact';
 import App from '../components/app';
 
 render(<App />, document.body);
+
+if ('serviceWorker' in navigator){
+  // var CACHE = 'preact-journal';
+
+  navigator.serviceWorker.onmessage = function (eTag) {
+    // var message = JSON.parse(e.data);
+
+    // var isRefresh = message.type === 'refresh';
+    // var isAsset = message.url.includes('asset');
+    var lastETag = localStorage.getItem('currentETag');
+    var isNew =  lastETag !== eTag;
+
+    if (/*isRefresh && isAsset &&*/ isNew) {
+      // if (lastETag) {
+      //   notice.hidden = false;
+      // }
+      localStorage.setItem('currentETag', eTag);
+      // location.reload();
+    }
+  };
+}
+
--- assets/js/sw.js
+++ assets/js/sw.js
@@ -1,112 +1,45 @@
 let version;
-
-self.addEventListener('install', function(e) {
-  self.skipWaiting();
-  e.waitUntil(
-    caches.open('preact-journal').then(function(cache) {
-      return cache.addAll(['/'])
-    })
-  )
-})
-
-self.addEventListener('fetch', function(e) {
-  if(e.request.method !== 'GET') return
-  if(e.request.url.indexOf('/api') > -1) return
-  
-  // All routes return the same payload. As such, cache only '/'
-  // and return its cached value on all routes.
-  var reqUrl
-  var url = e.request.url
-  if(~url.indexOf('/entries') || ~url.indexOf('/entry/')){
-    reqUrl = '/';
-  }
-
-  e.respondWith(
-    caches.match(reqUrl || e.request)
-    .then(response => response
-      ? response
-      : fetch(e.request)
-    )
-  )
-})
-
-
-
-
-
-
-
-
-
-// let version;
-// const CACHE = 'preact-journal';
-
-// self.addEventListener('message', function (e) {
-//   var data = e.data;
-//   if (!data) return;
-
-//   switch (data.action) {
-//     case 'skipWaiting': self.skipWaiting(); break;
-//   }
-// });
-
-// self.addEventListener('install', function(e) {
-//   self.skipWaiting();
-//   e.waitUntil(caches.open(CACHE).then(function (cache) {
-//     return cache.addAll(['/'])
-//   }));
-// });
-
-// self.addEventListener('fetch', function(e) {
-//   if(e.request.method !== 'GET') return
-//   if(e.request.url.indexOf('/api') > -1) return
-  
-//   // All routes return the same payload. As such, cache only '/'
-//   // and return its cached value on all routes.
-//   var reqUrl
-//   var url = e.request.url
-//   if(~url.indexOf('/entries') || ~url.indexOf('/entry/')){
-//     reqUrl = '/';
-//   }
-
-//   e.respondWith(fromCache(reqUrl || e.request));
+var CACHE = 'preact-journal';
  
-//   // e.waitUntil(
-//   //   update(reqUrl || e.request)
-//   //   .then(refresh)
-//   // );
-// });
+self.addEventListener('install', function(evt) {
+  evt.waitUntil(caches.open(CACHE).then(function (cache) {
+    cache.addAll(['/']);
+  }));
+});
  
-// function fromCache(request) {
-//   return caches.open(CACHE).then(function (cache) {
-//     return cache.match(request);
-//   });
-// }
+self.addEventListener('fetch', function(evt) {
+  evt.respondWith(fromCache(evt.request));
+  evt.waitUntil(
+    update(evt.request)
+    .then(refresh)
+  );
+});
  
-// function update(request) {
-//   return caches.open(CACHE).then(function (cache) {
-//     return fetch(request).then(function (response) {
-//       return cache.put(request, response.clone()).then(function () {
-//         return response;
-//       });
-//     });
-//   });
-// }
+function fromCache(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request);
+  });
+}
  
-// function refresh(response) {
-//   return self.clients.matchAll().then(function (clients) {
-//     clients.forEach(function (client) {
+function update(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response.clone()).then(function () {
+        return response;
+      });
+    });
+  });
+}
  
-//       console.log('RESPONSE', response);
-//       console.log('SIZE', response.headers.get('Content-Length'));
-
-//       var message = {
-//         type: 'reload',
-//         url: response.url,
-//         eTag: response.headers.get('ETag')
-//       };
- 
-//       client.postMessage(JSON.stringify(message));
-//     });
-//   });
-// }
+function refresh(response) {
+  return self.clients.matchAll().then(function (clients) {
+    clients.forEach(function (client) {
+      // var message = {
+      //   // type: 'refresh',
+      //   // url: response.url,
+      //   eTag: response.headers.get('ETag')
+      // };
+      client.postMessage(response.headers.get('ETag'));
+    });
+  });
+}
