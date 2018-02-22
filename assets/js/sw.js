d40cc08ab1b64a4a46ba1b8ebf715a109e7d97dc
Joseph Podwys <joseph@podwys.com>
2018-02-21 19:02:24 -0700

    I do not understand why sw isn’t updating when I tell it to.

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -1,11 +1,13 @@
 let version;
-const CACHE = 'preact-journal';
- 
+
 self.addEventListener('install', function(e) {
-  e.waitUntil(caches.open(CACHE).then(function (cache) {
-    return cache.addAll(['/'])
-  }));
-});
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open('preact-journal').then(function(cache) {
+      return cache.addAll(['/'])
+    })
+  )
+})
 
 self.addEventListener('fetch', function(e) {
   if(e.request.method !== 'GET') return
@@ -19,41 +21,92 @@ self.addEventListener('fetch', function(e) {
     reqUrl = '/';
   }
 
-  e.respondWith(fromCache(e.request));
+  e.respondWith(
+    caches.match(reqUrl || e.request)
+    .then(response => response
+      ? response
+      : fetch(e.request)
+    )
+  )
+})
+
+
+
+
+
+
+
+
+
+// let version;
+// const CACHE = 'preact-journal';
+
+// self.addEventListener('message', function (e) {
+//   var data = e.data;
+//   if (!data) return;
+
+//   switch (data.action) {
+//     case 'skipWaiting': self.skipWaiting(); break;
+//   }
+// });
+
+// self.addEventListener('install', function(e) {
+//   self.skipWaiting();
+//   e.waitUntil(caches.open(CACHE).then(function (cache) {
+//     return cache.addAll(['/'])
+//   }));
+// });
+
+// self.addEventListener('fetch', function(e) {
+//   if(e.request.method !== 'GET') return
+//   if(e.request.url.indexOf('/api') > -1) return
+  
+//   // All routes return the same payload. As such, cache only '/'
+//   // and return its cached value on all routes.
+//   var reqUrl
+//   var url = e.request.url
+//   if(~url.indexOf('/entries') || ~url.indexOf('/entry/')){
+//     reqUrl = '/';
+//   }
+
+//   e.respondWith(fromCache(reqUrl || e.request));
  
-  e.waitUntil(
-    update(e.request)
-    .then(refresh)
-  );
-});
+//   // e.waitUntil(
+//   //   update(reqUrl || e.request)
+//   //   .then(refresh)
+//   // );
+// });
  
-function fromCache(request) {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.match(request);
-  });
-}
+// function fromCache(request) {
+//   return caches.open(CACHE).then(function (cache) {
+//     return cache.match(request);
+//   });
+// }
  
-function update(request) {
-  return caches.open(CACHE).then(function (cache) {
-    return fetch(request).then(function (response) {
-      return cache.put(request, response.clone()).then(function () {
-        return response;
-      });
-    });
-  });
-}
+// function update(request) {
+//   return caches.open(CACHE).then(function (cache) {
+//     return fetch(request).then(function (response) {
+//       return cache.put(request, response.clone()).then(function () {
+//         return response;
+//       });
+//     });
+//   });
+// }
  
-function refresh(response) {
-  return self.clients.matchAll().then(function (clients) {
-    clients.forEach(function (client) {
+// function refresh(response) {
+//   return self.clients.matchAll().then(function (clients) {
+//     clients.forEach(function (client) {
  
-      var message = {
-        type: 'reload',
-        url: response.url,
-        eTag: response.headers.get('ETag')
-      };
+//       console.log('RESPONSE', response);
+//       console.log('SIZE', response.headers.get('Content-Length'));
+
+//       var message = {
+//         type: 'reload',
+//         url: response.url,
+//         eTag: response.headers.get('ETag')
+//       };
  
-      client.postMessage(JSON.stringify(message));
-    });
-  });
-}
+//       client.postMessage(JSON.stringify(message));
+//     });
+//   });
+// }
