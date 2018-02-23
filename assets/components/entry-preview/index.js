3e6cb323f26de80e630db98a278b88861b2c64a6
Joseph Podwys <joseph@podwys.com>
2018-02-22 21:32:05 -0700

    Some basic entry preview UI updates.

--- assets/components/entry-preview/index.js
+++ assets/components/entry-preview/index.js
@@ -4,16 +4,23 @@ export default class EntryPreview extends Component {
   render({ entry }) {
     if(entry.deleted || entry.newEntry && !entry.postPending) return '';
     return (
-      <div class="entry-preview">
-        <a
-          class="entry-link"
-          href={"/entry/" + entry.id}>
-          {entry.date}
-        </a>
-        <span>{ !entry.isPublic ? '🔐' : '🔓' }</span>✖
-        <p class="entry-text">
-          {entry.text.length > 140 ? entry.text.substr(0, 140) + '...' : entry.text}
-        </p>
+      <div class="list-item">
+        <div class="first-row">
+          <a
+            class="entry-link"
+            href={"/entry/" + entry.id}>
+            {entry.date}
+          </a>
+
+          {/*<span>{ !entry.isPublic ? '🔐' : '🔓' }</span>✖*/}
+        </div>
+
+        <div class="second-row">
+          <p class="entry-text">
+            {/* {entry.text.length > 140 ? entry.text.substr(0, 140) + '...' : entry.text} */}
+            {entry.text}
+          </p>
+        </div>
       </div>
     );
   }
