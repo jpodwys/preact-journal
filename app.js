d60398e54ffe46347905ebf279592ea6630275eb
Joseph Podwys <joseph@podwys.com>
2018-03-05 15:09:39 -0700

    Repo cleanup. Remove unused reps and get rid of all critical CSS stuff.

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 require('dotenv').load();
 var express = require('express'),
   compress = require('compression'),
-  // shrinkRay = require('shrink-ray-current'),
   bodyParser = require('body-parser'),
   cookieParser = require('cookie-parser'),
   app = express(),
@@ -22,8 +21,6 @@ app.disable('x-powered-by');
 app.use(forceSsl);
 app.use(strictTransportSecurity);
 app.use(compress({threshold: '1.4kb'}));
-// app.use(shrinkRay({threshold: '1.4kb'}));
-// var maxAge = (process.env.NODE_ENV === 'production') ? '30d' : '0h';
 app.use(express.static('dist', {maxAge: '0h'}));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
--- gulpfile.js
+++ gulpfile.js
@@ -7,16 +7,8 @@ var inlinesource = require('gulp-inline-source');
 var htmlmin = require('gulp-htmlmin');
 var rename = require('gulp-rename');
 var webpack = require('gulp-webpack');
-var criticalCss = require('gulp-penthouse');
 var del = require('del');
 var replace = require('gulp-replace');
-// var rev = require('gulp-rev');
-// var revReplace = require('gulp-rev-replace');
-var PORT = process.env.PORT || 3000;
-var LOCAL = process.env.NODE_ENV === 'production';
-var CRITICAL_URL = process.env.NODE_ENV === 'production'
-  ? 'https://preact-journal.herokuapp.com/critical'
-  : 'http://localhost:' + PORT + '/critical';
 
 function serve(cb) {
   require('./app.js');
@@ -30,13 +22,6 @@ function scripts(cb) {
     .pipe(gulp.dest('./dist'));
 }
 
-function criticalScripts(cb) {
-  return gulp.src('assets/js/critical.js')
-    .pipe(webpack(require('./webpack-critical.config.babel.js')))
-    .pipe(rename('critical-bundle.js'))
-    .pipe(gulp.dest('./dist'));
-}
-
 function sw() {
   return gulp.src('assets/js/sw.js')
     .pipe(replace('let version;', 'let version = ' + Date.now() + ';'))
@@ -44,53 +29,13 @@ function sw() {
       presets: ['env']
     }))
     .pipe(uglify())
-    // .pipe(rev())
-    // .pipe(gulp.dest('./dist'))
-    // .pipe(rev.manifest())
-    .pipe(gulp.dest('./dist'));
-    
+    .pipe(gulp.dest('./dist'));   
 }
-
-// function replaceRevRef() {
-//   var manifest = gulp.src('dist/rev-manifest.json');
-
-//   return gulp.src('assets/index.html')
-//     .pipe(revReplace({manifest: manifest}))
-//     .pipe(gulp.dest('./dist'));
-// }
 
 function images() {
   return gulp.src('assets/images/**.*')
     .pipe(gulp.dest('./dist'));
 }
-
-// function styles() {
-//   return gulp.src('assets/css/styles.css')
-//     .pipe(criticalCss({
-//       width: 768,
-//       height: 10000,
-//       keepLargerMediaQueries: true,
-//       url: CRITICAL_URL,
-//       pageLoadSkipTimeout: 5000,
-//       blockJSRequests: false,
-//       renderWaitTime: 1000,
-//       propertiesToRemove: [
-//         'pointer-events',
-//         '(-webkit-)?tap-highlight-color',
-//         '(.*)user-select'
-//       ],
-//       forceInclude: [
-//         '.search-icon:hover',
-//         '(.*)transition(.*)',
-//         'a:not(.pure-menu-link):hover',
-//         '.pure-menu-link:hover',
-//         '.pure-menu-link.active'
-//       ],
-//       out: 'styles.css'
-//     }))
-//     .pipe(cleanCSS({compatibility: 'ie8'}))
-//     .pipe(gulp.dest('./dist'));
-// }
 
 function styles() {
   return gulp.src('assets/css/styles.css')
@@ -103,10 +48,6 @@ function moveStyles() {
     .pipe(rename('styles.css'))
     .pipe(gulp.dest('./dist'));
 }
-
-// function clean() {
-//   return del(['cssstyles.css']);
-// }
 
 function clean() {
   return del(['./dist']);
@@ -125,20 +66,12 @@ function inline() {
     .pipe(gulp.dest('./dist'));
 }
 
-// function watch() {
-//   gulp.watch(paths.scripts.src, scripts);
-//   gulp.watch(paths.styles.src, styles);
-// }
-
 function build() {
   return gulp.series(
     clean,
     serve,
-    gulp.parallel(scripts, criticalScripts, sw, images),
+    gulp.parallel(scripts, sw, images),
     styles,
-    // moveStyles,
-    // gulp.parallel(clean, inline)
-    // replaceRevRef,
     inline
   )();
 }
