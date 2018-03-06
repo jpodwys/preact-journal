b6216349a1ae6bbdcfd52b2d0a4c1e163f2d0731
Joseph Podwys <joseph@podwys.com>
2018-03-06 15:32:28 -0700

    no message

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -7,74 +7,83 @@ self.addEventListener('install', function(e) {
   }));
 });
 
-// self.addEventListener('fetch', event => {
-//   // Skip cross-origin requests, like those for Google Analytics.
-//   if (event.request.url.startsWith(self.location.origin)) {
-//     if(event.request.method !== 'GET') return
-//     if(event.request.url.indexOf('/api') > -1) return
-//     // if(event.request.url.indexOf('/manifest.json') > -1) return
-//     event.respondWith(
-//       caches.match(event.request).then(cachedResponse => {
-//         if (cachedResponse) {
-//           return cachedResponse;
-//         }
+self.addEventListener('fetch', event => {
+  // Skip cross-origin requests, like those for Google Analytics.
+  if (event.request.url.startsWith(self.location.origin)) {
+    if(event.request.method !== 'GET') return
+    if(event.request.url.indexOf('/api') > -1) return
+    // if(event.request.url.indexOf('/manifest.json') > -1) return
 
-//         return caches.open(CACHE).then(cache => {
-//           return fetch(event.request).then(response => {
-//             // Put a copy of the response in the runtime cache.
-//             return cache.put(event.request, response.clone()).then(() => {
-//               return response;
-//             });
-//           });
-//         });
-//       })
-//     );
-//   }
-// });
- 
-self.addEventListener('fetch', function(e) {
-  if(e.request.method !== 'GET') return
-  if(e.request.url.indexOf('/api') > -1) return
-  // if(e.request.url.indexOf('/manifest.json') > -1) return
-  
-  // All routes return the same payload. As such, cache only '/'
-  // and return its cached value on all routes.
-  let reqUrl;
-  let url = e.request.url
-  if(~url.indexOf('/entries') || ~url.indexOf('/entry/')){
-    reqUrl = '/';
+    // All routes return the same payload. As such, cache only '/'
+    // and return its cached value on all routes.
+    let reqUrl;
+    let url = e.request.url
+    if(~url.indexOf('/entries') || ~url.indexOf('/entry/')){
+      reqUrl = '/';
+    }
+
+    event.respondWith(
+      caches.match(reqUrl || event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return caches.open(CACHE).then(cache => {
+          return fetch(event.request).then(response => {
+            // Put a copy of the response in the runtime cache.
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
   }
-
-  e.respondWith(fromCache(reqUrl || e.request));
-
-  e.waitUntil(
-    update(reqUrl || e.request)
-    .then(refresh)
-  );
 });
  
-function fromCache(request) {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.match(request);
-  });
-}
+// self.addEventListener('fetch', function(e) {
+//   if(e.request.method !== 'GET') return
+//   if(e.request.url.indexOf('/api') > -1) return
+//   if(e.request.url.indexOf('/manifest.json') > -1) return
+  
+  // // All routes return the same payload. As such, cache only '/'
+  // // and return its cached value on all routes.
+  // let reqUrl;
+  // let url = e.request.url
+  // if(~url.indexOf('/entries') || ~url.indexOf('/entry/')){
+  //   reqUrl = '/';
+  // }
+
+//   e.respondWith(fromCache(reqUrl || e.request));
+
+//   e.waitUntil(
+//     update(reqUrl || e.request)
+//     .then(refresh)
+//   );
+// });
  
-function update(request) {
-  return caches.open(CACHE).then(function (cache) {
-    return fetch(request).then(function (response) {
-      if(response.status >= 300) return;
-      return cache.put(request, response.clone()).then(function () {
-        return response;
-      });
-    });
-  });
-}
+// function fromCache(request) {
+//   return caches.open(CACHE).then(function (cache) {
+//     return cache.match(request);
+//   });
+// }
  
-function refresh(response) {
-  if(!response) return;
-  return self.clients.matchAll().then(function (clients) {
-    clients.forEach(function (client) {
-      client.postMessage(response.headers.get('ETag'));
-    });
-  });
-}
+// function update(request) {
+//   return caches.open(CACHE).then(function (cache) {
+//     return fetch(request).then(function (response) {
+//       if(response.status >= 300) return;
+//       return cache.put(request, response.clone()).then(function () {
+//         return response;
+//       });
+//     });
+//   });
+// }
+ 
+// function refresh(response) {
+//   if(!response) return;
+//   return self.clients.matchAll().then(function (clients) {
+//     clients.forEach(function (client) {
+//       client.postMessage(response.headers.get('ETag'));
+//     });
+//   });
+// }
