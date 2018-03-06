b8a244c5aa84ada1d3f1f68751f4b3ab6574d441
Joseph Podwys <joseph@podwys.com>
2018-03-06 15:00:22 -0700

    no message

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -6,27 +6,49 @@ self.addEventListener('install', function(e) {
     cache.addAll(['/']);
   }));
 });
- 
-self.addEventListener('fetch', function(e) {
-  if(e.request.method !== 'GET') return
-  if(e.request.url.indexOf('/api') > -1) return
-  if(e.request.url.indexOf('/manifest.json') > -1) return
-  
-  // All routes return the same payload. As such, cache only '/'
-  // and return its cached value on all routes.
-  // let reqUrl;
-  // let url = e.request.url
-  // if(~url.indexOf('/entries') || ~url.indexOf('/entry/')){
-  //   reqUrl = '/';
-  // }
 
-  e.respondWith(fromCache(reqUrl || e.request));
+self.addEventListener('fetch', event => {
+  // Skip cross-origin requests, like those for Google Analytics.
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
 
-  // e.waitUntil(
-  //   update(reqUrl || e.request)
-  //   .then(refresh)
-  // );
+        return caches.open(CACHE).then(cache => {
+          return fetch(event.request).then(response => {
+            // Put a copy of the response in the runtime cache.
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
 });
+ 
+// self.addEventListener('fetch', function(e) {
+//   if(e.request.method !== 'GET') return
+//   if(e.request.url.indexOf('/api') > -1) return
+//   if(e.request.url.indexOf('/manifest.json') > -1) return
+  
+//   // All routes return the same payload. As such, cache only '/'
+//   // and return its cached value on all routes.
+//   // let reqUrl;
+//   // let url = e.request.url
+//   // if(~url.indexOf('/entries') || ~url.indexOf('/entry/')){
+//   //   reqUrl = '/';
+//   // }
+
+//   e.respondWith(fromCache(reqUrl || e.request));
+
+//   // e.waitUntil(
+//   //   update(reqUrl || e.request)
+//   //   .then(refresh)
+//   // );
+// });
  
 function fromCache(request) {
   return caches.open(CACHE).then(function (cache) {
