4f74cf9a66d543e0c764f95162c4574df86a1d53
Joseph Podwys <joseph@podwys.com>
2018-03-06 15:09:04 -0700

    no message

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -10,6 +10,9 @@ self.addEventListener('install', function(e) {
 self.addEventListener('fetch', event => {
   // Skip cross-origin requests, like those for Google Analytics.
   if (event.request.url.startsWith(self.location.origin)) {
+    if(event.request.method !== 'GET') return
+    if(event.request.url.indexOf('/api') > -1) return
+    if(event.request.url.indexOf('/manifest.json') > -1) return
     event.respondWith(
       caches.match(event.request).then(cachedResponse => {
         if (cachedResponse) {
@@ -30,48 +33,48 @@ self.addEventListener('fetch', event => {
 });
  
 // self.addEventListener('fetch', function(e) {
-//   if(e.request.method !== 'GET') return
-//   if(e.request.url.indexOf('/api') > -1) return
-//   if(e.request.url.indexOf('/manifest.json') > -1) return
+  // if(e.request.method !== 'GET') return
+  // if(e.request.url.indexOf('/api') > -1) return
+  // if(e.request.url.indexOf('/manifest.json') > -1) return
   
 //   // All routes return the same payload. As such, cache only '/'
 //   // and return its cached value on all routes.
-//   // let reqUrl;
-//   // let url = e.request.url
-//   // if(~url.indexOf('/entries') || ~url.indexOf('/entry/')){
-//   //   reqUrl = '/';
-//   // }
+//   let reqUrl;
+//   let url = e.request.url
+//   if(~url.indexOf('/entries') || ~url.indexOf('/entry/')){
+//     reqUrl = '/';
+//   }
 
 //   e.respondWith(fromCache(reqUrl || e.request));
 
-//   // e.waitUntil(
-//   //   update(reqUrl || e.request)
-//   //   .then(refresh)
-//   // );
+//   e.waitUntil(
+//     update(reqUrl || e.request)
+//     .then(refresh)
+//   );
 // });
  
-function fromCache(request) {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.match(request);
-  });
-}
+// function fromCache(request) {
+//   return caches.open(CACHE).then(function (cache) {
+//     return cache.match(request);
+//   });
+// }
  
-function update(request) {
-  return caches.open(CACHE).then(function (cache) {
-    return fetch(request).then(function (response) {
-      if(response.status >= 300) return;
-      return cache.put(request, response.clone()).then(function () {
-        return response;
-      });
-    });
-  });
-}
+// function update(request) {
+//   return caches.open(CACHE).then(function (cache) {
+//     return fetch(request).then(function (response) {
+//       if(response.status >= 300) return;
+//       return cache.put(request, response.clone()).then(function () {
+//         return response;
+//       });
+//     });
+//   });
+// }
  
-function refresh(response) {
-  if(!response) return;
-  return self.clients.matchAll().then(function (clients) {
-    clients.forEach(function (client) {
-      client.postMessage(response.headers.get('ETag'));
-    });
-  });
-}
+// function refresh(response) {
+//   if(!response) return;
+//   return self.clients.matchAll().then(function (clients) {
+//     clients.forEach(function (client) {
+//       client.postMessage(response.headers.get('ETag'));
+//     });
+//   });
+// }
