1a906f3c2f0646d0e78e5c718317458d5783d376
Joseph Podwys <joseph@podwys.com>
2018-03-15 14:46:18 -0600

    Keep the dyno awake.

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ var express = require('express'),
   PORT = process.env.PORT || 3000;
 
 // Keep the dyno awake
-// var http = require('http');
-// setInterval(function() {
-//   http.get('https://riot-demo.herokuapp.com');
-// }, 900000); // Every 15 minutes
+var http = require('http');
+setInterval(function() {
+  http.get('https://riot-demo.herokuapp.com');
+}, 900000); // Every 15 minutes
 
 app.disable('x-powered-by');
 app.use(forceSsl);
