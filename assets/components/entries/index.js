840b53db3024a9f57428fc78bcf50938b486df14
Joseph Podwys <joseph@podwys.com>
2018-04-24 10:12:00 -0600

    More fun

--- assets/components/entries/index.js
+++ assets/components/entries/index.js
@@ -25,7 +25,7 @@ export default class Entries extends Component {
       );
     }
     return (
-      <Transition className="reveal">
+      <Transition className="fly">
         <ScrollViewport class="entry-list" rowHeight={84} overscan={20}>
           {entries.map(entry => <EntryPreview entry={entry}/>)}
         </ScrollViewport>
