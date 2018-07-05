a02a7259b8bcbd1182065b197724266c31dd801f
Joseph Podwys <joseph@podwys.com>
2018-07-05 00:50:36 -0600

    Remove extra assignment line.

--- client/js/app-state/index.js
+++ client/js/app-state/index.js
@@ -4,12 +4,10 @@ import { clearLocalStorage, getViewFromHref, applyFilters } from '../utils';
 export default function getInitialState () {
   let loggedIn = !!cookie.get('logged_in');
   if(!loggedIn) clearLocalStorage();
-  let entries = JSON.parse(localStorage.getItem('entries')) || undefined;
 
   let state = {
     entryIndex: -1,
     filterText: '',
-    entries: entries,
     entry: undefined,
     scrollPosition: 0,
     loggedIn: loggedIn,
@@ -17,6 +15,7 @@ export default function getInitialState () {
     toastConfig: undefined,
     view: getViewFromHref(location.href),
     dark: localStorage.getItem('dark') === 'true',
+    entries: JSON.parse(localStorage.getItem('entries')) || undefined,
     get viewEntries() { return applyFilters(this.filterText, this.entries); },
     set viewEntries() { return console.warn('Attempted to set computed property viewEntries.'); }
   };
