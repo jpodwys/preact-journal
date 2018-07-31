e488074eb72a2b0ba813265f8ec54eaaf26aee90
Joseph Podwys <joseph@podwys.com>
2018-07-31 10:00:04 -0600

    Changing from bland purple to light blue.

--- client/components/entries/index.js
+++ client/components/entries/index.js
@@ -26,7 +26,7 @@ export default class Entries extends Component {
     entries = entries || [];
     if(!entries.length){
       return (
-        <h2 class="center-text">It's empty in here!</h2>
+        <h2 class="center-text fade-up">It's empty in here!</h2>
       );
     }
     return (
