fd7c6b3d9c71835342a56d12c9993517e80b457d
Joseph Podwys <jpodwys@Joseph-Podwys-MacBook-Pro.local>
2018-08-31 17:09:47 -0600

    Putting it back now.

--- client/js/index.js
+++ client/js/index.js
@@ -4,7 +4,7 @@ import App from '../components/app';
 render(<App />, document.body);
 
 if('serviceWorker' in navigator){
-  // navigator.serviceWorker.register('/sw.js');
+  navigator.serviceWorker.register('/sw.js');
   
   // navigator.serviceWorker.onmessage = function (e) {
   //   let eTag = e.data;
