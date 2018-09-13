9d9b931e7439c942e65958996e1c97485140a9bb
Joseph Podwys <jpodwys@Joseph-Podwys-MacBook-Pro.local>
2018-09-13 17:40:26 -0600

    Laying the groundwork for coverage reporting.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -4,9 +4,16 @@ module.exports = function(config) {
   config.set({
     basePath: '../',
     frameworks: [ 'mocha', 'chai', 'sinon' ],
-    reporters: [ 'mocha' ],
+    reporters: [ 'mocha', /*'progress',*/ 'coverage' ],
     browsers: [ 'ChromeHeadless' ],
     files: [ 'client/js/**/*.spec.js' ],
-    preprocessors: { 'client/js/**/*.js': [ 'webpack' ] }
+    preprocessors: { 'client/js/**/*.js': [ 'webpack', 'coverage' ] },
+    /* Reporting is way off right now. */
+    // coverageReporter: {
+    //   reporters: [
+    //     { type: 'text' },
+    //     { type: 'html' }
+    //   ]
+    // }
   });
 };
