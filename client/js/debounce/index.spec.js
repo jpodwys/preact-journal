70aceb4ea158c3975c11c475512114f80fa96877
Joseph Podwys <jpodwys@josephpodwysmbp.lan>
2018-09-13 19:36:27 -0600

    Improving debounce function coverage.

--- client/js/debounce/index.spec.js
+++ client/js/debounce/index.spec.js
@@ -1,9 +1,18 @@
 import debounce from './index';
 
 describe('debounce', () => {
+  const cb = sinon.spy();
 
   it('should return a function', () => {
     expect(typeof debounce()).to.equal('function');
+  });
+
+  it('should execute the passed callback', (done) => {
+    debounce(cb)();
+    setTimeout(() => {
+      expect(cb.calledOnce).to.be.true;
+      done();
+    });
   });
   
 });
