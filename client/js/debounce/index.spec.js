6aea5fbd0191a57f1efa57074bb4bc73b382b581
Joe Podwys <joe@podwys.com>
2018-09-14 16:32:55 -0600

    Removing unused code branches and hit 100% (for these few files). (#37)
    * Improving tests
    
    * Getting to 100% coverage.
    
    * Adding tests to arrow.
    
    * Covering arrow even better.

--- client/js/debounce/index.spec.js
+++ client/js/debounce/index.spec.js
@@ -1,7 +1,15 @@
 import debounce from './index';
 
 describe('debounce', () => {
-  const cb = sinon.spy();
+  let cb;
+
+  beforeEach(() => {
+    cb = sinon.spy();
+  });
+
+  afterEach(() => {
+    cb.resetHistory();
+  });
 
   it('should return a function', () => {
     expect(typeof debounce()).to.equal('function');
@@ -12,6 +20,17 @@ describe('debounce', () => {
     setTimeout(() => {
       expect(cb.calledOnce).to.be.true;
       done();
+    });
+  });
+
+  it('should wait the passed delay to execute the passed callback', (done) => {
+    debounce(cb, 10)();
+    setTimeout(() => {
+      expect(cb.calledOnce).to.be.false;
+      setTimeout(() => {
+        expect(cb.calledOnce).to.be.true;
+        done();
+      }, 10)
     });
   });
   
