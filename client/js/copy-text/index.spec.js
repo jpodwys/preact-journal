a17c3c045b64a0ff5c9c67bf2f0706e76dee9e67
Joseph Podwys <jpodwys@josephpodwysmbp.lan>
2018-09-27 19:56:44 -0600

    copyText tests

--- client/js/copy-text/index.spec.js
+++ client/js/copy-text/index.spec.js
@@ -1,7 +1,22 @@
 import copyText from './index';
   
 describe('copyText', () => {
-  it('should', () => {
-    expect(true).to.be.true;
+
+  it('should fire linkstate', (done) => {
+    const cb = sinon.spy();
+    document.execCommand = () => true;
+    document.addEventListener('linkstate', cb);
+    copyText('bogus');
+    setTimeout(() => {
+      expect(cb.called).to.be.true;
+      done();
+    });
   });
+
+  it('should use the share API when available', () => {
+    navigator.share = sinon.spy();
+    copyText('bogus');
+    expect(navigator.share.args[0][0].text).to.equal('bogus');
+  });
+  
 });
