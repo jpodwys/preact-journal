d1eafa1b8848ef46abc6db0856362d2947d52e68
Joseph Podwys <jpodwys@josephpodwysmbp.lan>
2018-11-10 00:33:04 -0700

    SW cleanup

--- client/js/sw.js
+++ client/js/sw.js
@@ -1,12 +1,12 @@
 const CACHE = 'preact-journal';
  
-self.addEventListener('install', function(e) {
-  e.waitUntil(caches.open(CACHE).then(function (cache) {
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(cache => {
     cache.addAll(['/', '/manifest.json', '/version', '/favicon-16.ico', '/icon-512x512.png']);
   }));
 });
  
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', e => {
   if(e.request.method !== 'GET' && !~e.request.url.indexOf('/version')) return
   if(~e.request.url.indexOf('/api')) return
   
@@ -28,15 +28,15 @@ self.addEventListener('fetch', function(e) {
 });
  
 function fromCache(request) {
-  return caches.open(CACHE).then(function (cache) {
+  return caches.open(CACHE).then(cache => {
     return cache.match(request);
   });
 }
  
 function update(request) {
-  // Fetch new static json asset with the version number
-  // If it differs from the one stored, store the new one and fetch and store the new index.html
-  return caches.open(CACHE).then(function (cache) {
+  // Fetch app version number. If it differs from the one stored,
+  // store the new one and fetch and store the new index.html
+  return caches.open(CACHE).then(cache => {
     let versionRes;
     return fetch('/version')
       .then(res => {
@@ -47,40 +47,23 @@ function update(request) {
         cache.match('/version')
           .then(res => res.json())
           .then(match => {
-            console.log('version', version);
-            console.log('match', match);
             if(version.version === match.version) return;
             cache.put('/version', versionRes.clone());
-            return fetch('/').then(function (res) {
+            return fetch('/').then(res => {
               if(res.status >= 300) return;
-              return cache.put('/', res.clone()).then(function () {
+              return cache.put('/', res.clone()).then(() => {
                 return res;
               });
             });
-            // return fetch(request).then(function (res) {
-            //   if(res.status >= 300) return;
-            //   return cache.put(request, res.clone()).then(function () {
-            //     return res;
-            //   });
-            // });
       });
     }).catch(err => console.log(err));
   });
-
-  // return caches.open(CACHE).then(function (cache) {
-    // return fetch(request).then(function (response) {
-    //   if(response.status >= 300) return;
-    //   return cache.put(request, response.clone()).then(function () {
-    //     return response;
-    //   });
-    // });
-  // });
 }
  
 // function refresh(response) {
 //   if(!response) return;
-//   return self.clients.matchAll().then(function (clients) {
-//     clients.forEach(function (client) {
+//   return self.clients.matchAll().then(clients => {
+//     clients.forEach(client => {
 //       client.postMessage(response.headers.get('ETag'));
 //     });
 //   });
