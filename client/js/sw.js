acd6ccda1b72280e1f5bd6971e35031caf7068ad
Joe Podwys <joe@podwys.com>
2018-11-11 13:44:17 -0700

    ServiceWorker Efficiency (#45)
    * Making it so I no longer download the entire site to check if there are changes.
    
    * Testing the sw impl.
    
    * It worked!!
    
    * SW cleanup

--- client/js/sw.js
+++ client/js/sw.js
@@ -1,14 +1,13 @@
-let version;
-let CACHE = 'preact-journal';
+const CACHE = 'preact-journal';
  
-self.addEventListener('install', function(e) {
-  e.waitUntil(caches.open(CACHE).then(function (cache) {
-    cache.addAll(['/', '/manifest.json', '/favicon-16.ico', '/icon-512x512.png']);
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(cache => {
+    cache.addAll(['/', '/manifest.json', '/version', '/favicon-16.ico', '/icon-512x512.png']);
   }));
 });
  
-self.addEventListener('fetch', function(e) {
-  if(e.request.method !== 'GET') return
+self.addEventListener('fetch', e => {
+  if(e.request.method !== 'GET' && !~e.request.url.indexOf('/version')) return
   if(~e.request.url.indexOf('/api')) return
   
   // All routes return the same payload. As such, cache only '/'
@@ -29,26 +28,42 @@ self.addEventListener('fetch', function(e) {
 });
  
 function fromCache(request) {
-  return caches.open(CACHE).then(function (cache) {
+  return caches.open(CACHE).then(cache => {
     return cache.match(request);
   });
 }
  
 function update(request) {
-  return caches.open(CACHE).then(function (cache) {
-    return fetch(request).then(function (response) {
-      if(response.status >= 300) return;
-      return cache.put(request, response.clone()).then(function () {
-        return response;
+  // Fetch app version number. If it differs from the one stored,
+  // store the new one and fetch and store the new index.html
+  return caches.open(CACHE).then(cache => {
+    let versionRes;
+    return fetch('/version')
+      .then(res => {
+        versionRes = res.clone();
+        return res.json();
+      })
+      .then(version => {
+        cache.match('/version')
+          .then(res => res.json())
+          .then(match => {
+            if(version.version === match.version) return;
+            cache.put('/version', versionRes.clone());
+            return fetch('/').then(res => {
+              if(res.status >= 300) return;
+              return cache.put('/', res.clone()).then(() => {
+                return res;
+              });
+            });
       });
-    });
+    }).catch(err => console.log(err));
   });
 }
  
 // function refresh(response) {
 //   if(!response) return;
-//   return self.clients.matchAll().then(function (clients) {
-//     clients.forEach(function (client) {
+//   return self.clients.matchAll().then(clients => {
+//     clients.forEach(client => {
 //       client.postMessage(response.headers.get('ETag'));
 //     });
 //   });
