b73d7194dbf8e316da475420d1e95ff12e50180d
Joseph Podwys <joseph@podwys.com>
2019-01-06 18:01:44 -0700

    Hide on touchstart too

--- client/components/dialog/index.js
+++ client/components/dialog/index.js
@@ -1,9 +1,14 @@
 import { h } from 'preact';
 import { fire } from '../unifire';
 
+const hideDialog = fire('linkstate', {key: 'dialogMode'});
+
 export default ({ dialogMode, children }) => (
-  <div class={`modal-overlay modal-${dialogMode} fade-in`} onclick={fire('linkstate', {key: 'dialogMode'})}>
-    <div class={`modal-dialog modal-${dialogMode} grow`}>
+  <div
+    class={`modal-overlay modal-${dialogMode} fade-in`}
+    onclick={hideDialog}
+    ontouchstart={hideDialog}>
+    <div class={`modal-dialog modal-${dialogMode} grow`} ontouchstart={e => e.stopPropagation()}>
       { children[0] }
     </div>
   </div>
