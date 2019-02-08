21404385544e1db4988fe1705df49f9a99054cdd
Joseph Podwys <joseph@podwys.com>
2019-02-07 22:13:45 -0700

    Converting more classes to functions.

--- client/components/entry-preview/index.js
+++ client/components/entry-preview/index.js
@@ -1,45 +1,39 @@
-import { h, Component } from 'preact';
+import { h } from 'preact';
 import Icon from '../icon';
 import { fire } from '../unifire';
 import copyText from '../../js/copy-text';
 
-export default class EntryPreview extends Component {
-  copy = () => {
-    copyText(this.props.entry.date + ' ' + this.props.entry.text);
-  }
+const copy = ({ date, text }) => copyText(date + ' ' + text);
 
-  getText(entry) {
-    return entry.previewText || entry.text;
-  }
+const getText = ({ previewText, text }) => previewText || text;
 
-  render({ entry }) {
-    const favoriteIcon = entry && entry.favorited ? 'star-filled' : 'star-empty';
-    const fadeRight = entry.slideIn ? 'fade-right' : '';
-    if(fadeRight) setTimeout(fire('removeSlideInProp'), 450);
+export default ({ entry }) => {
+  const favoriteIcon = entry && entry.favorited ? 'star-filled' : 'star-empty';
+  const fadeRight = entry.slideIn ? 'fade-right' : '';
+  if(fadeRight) setTimeout(fire('removeSlideInProp'), 450);
 
-    return (
-      <div class={`entry-preview ${fadeRight}`}>
-        <a href={"/entry/" + entry.id}>
-          <div class="list-item">
-            <div class="first-row">
-              <span
-                class="entry-link">
-                {entry.date}
-              </span>
+  return (
+    <div class={`entry-preview ${fadeRight}`}>
+      <a href={"/entry/" + entry.id}>
+        <div class="list-item">
+          <div class="first-row">
+            <span
+              class="entry-link">
+              {entry.date}
+            </span>
 
-            </div>
-
-            <div class="second-row">
-              {this.getText(entry)}
-            </div>
           </div>
-        </a>
 
-        <span class="nav-set right dark-fill entry-preview--icons">
-          <Icon icon="copy" key={entry.id + 'copy'} onclick={this.copy}/>
-          <Icon icon={favoriteIcon} onclick={fire('toggleFavorite', { id: entry.id, favorited: !entry.favorited })}/>
-        </span>
-      </div>
-    );
-  }
+          <div class="second-row">
+            {getText(entry)}
+          </div>
+        </div>
+      </a>
+
+      <span class="nav-set right dark-fill entry-preview--icons">
+        <Icon icon="copy" key={entry.id + 'copy'} onclick={() => copy(entry)}/>
+        <Icon icon={favoriteIcon} onclick={fire('toggleFavorite', { id: entry.id, favorited: !entry.favorited })}/>
+      </span>
+    </div>
+  );
 }
--- client/components/login/index.js
+++ client/components/login/index.js
@@ -1,52 +1,51 @@
-import { h, Component } from 'preact';
+import { h } from 'preact';
 import { fire } from '../../components/unifire';
 
-export default class Login extends Component {
-  componentWillUnmount() {
-    ['cuser', 'cpass', 'luser', 'lpass'].forEach(selector => {
-      this.base.querySelector('#' + selector).value = '';
-    });
-  }
+const getUser = prefix => ({
+  username: document.getElementById(prefix + 'user').value,
+  password: document.getElementById(prefix + 'pass').value
+})
 
-  getUser = prefix => ({
-    username: this.base.querySelector('#' + prefix + 'user').value,
-    password: this.base.querySelector('#' + prefix + 'pass').value
-  })
+const join = e => {
+  e.preventDefault();
+  fire('createAccount', getUser('c'))();
+}
 
-  join = e => {
-    e.preventDefault();
-    fire('createAccount', this.getUser('c'))();
-  }
+const login = e => {
+  e.preventDefault();
+  fire('login', getUser('l'))();
+}
 
-  login = e => {
-    e.preventDefault();
-    fire('login', this.getUser('l'))();
-  }
-
-  render() {
-    return (
-      <div class="login-page-wrapper">
-        <h1 class="center-text">Journalize</h1>
-        <h4 class="center-text">Private journal entries</h4>
-          <div class="login-form-wrapper">
-            <form onsubmit={this.join} class="full-width full-width--all">
-              <fieldset>
-                <legend>Create an Account</legend>
-                <input id="cuser" placeholder="username" autocapitalize="off"/>
-                <input id="cpass" type="password" placeholder="password"/>
-                <input type="submit" value="Submit"/>
-              </fieldset>
-            </form>
-            <form onsubmit={this.login} class="full-width full-width--all">
-              <fieldset>
-                <legend>or Login</legend>
-                <input id="luser" placeholder="username" autocapitalize="off"/>
-                <input id="lpass" type="password" placeholder="password"/>
-                <input type="submit" value="Submit"/>
-              </fieldset>
-            </form>
-          </div>
-      </div>
-    );
-  }
+export default () => {
+  /**
+   * For some reason, the input values are still there when this
+   * component unrenders then re-renders. Using dynamic keys fixes
+   * the problem. (Used componentWillUnmount before switching to a
+   * functional component.)
+   */
+  const now = Date.now();
+  return (
+    <div class="login-page-wrapper">
+      <h1 class="center-text">Journalize</h1>
+      <h4 class="center-text">Private journal entries</h4>
+        <div class="login-form-wrapper">
+          <form onsubmit={join} class="full-width full-width--all">
+            <fieldset>
+              <legend>Create an Account</legend>
+              <input id="cuser" placeholder="username" autocapitalize="off" key={now + 1}/>
+              <input id="cpass" type="password" placeholder="password" key={now + 2}/>
+              <input type="submit" value="Submit"/>
+            </fieldset>
+          </form>
+          <form onsubmit={login} class="full-width full-width--all">
+            <fieldset>
+              <legend>or Login</legend>
+              <input id="luser" placeholder="username" autocapitalize="off" key={now + 3}/>
+              <input id="lpass" type="password" placeholder="password" key={now + 4}/>
+              <input type="submit" value="Submit"/>
+            </fieldset>
+          </form>
+        </div>
+    </div>
+  );
 }
