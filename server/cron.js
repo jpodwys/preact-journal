62c6c36e92f90fb1d52ef8c0275293e0930bf1c9
Joseph Podwys <joseph@podwys.com>
2019-03-12 23:55:16 -0600

    import node's https module

--- server/cron.js
+++ server/cron.js
@@ -1,12 +1,12 @@
 module.exports = function(){
   // Keep the dyno awake when in production
   if(process.env.NODE_ENV === 'production'){
-    var https = require('http');
+    var https = require('https');
     setInterval(function() {
       https.get('https://preact-journal.herokuapp.com');
     }, 900000); // Every 15 minutes
   }
-  
+
   // On the 31st of the 7 months/year that have 31 days,
   // remove all entries marked as deleted in the database.
   // I wait 31+ days because I need to outlast a maximum
@@ -16,7 +16,7 @@ module.exports = function(){
       db = require('./db')(Sequelize),
       entryModel = require('./models/entry-model')(db, Sequelize),
       entryService = new (require('./services/entry-service'))(entryModel, db);
-    
+
     entryService.removeAllDeletedEntries();
   }
 }
