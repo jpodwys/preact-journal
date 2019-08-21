12f69b2f14f9473908bc95a030eb2e5ff66cb995
Joseph Podwys <joseph@podwys.com>
2019-08-21 01:05:05 -0600

    Simplification

--- server/utils/date.js
+++ server/utils/date.js
@@ -1,16 +1,15 @@
-const oneMonth = 60 * 60 * 1000 * 24 * 30;
 const thirtyOneDaysInSeconds = 31 * 24 * 60 * 60;
 
 const getTimestampFromThirtyOneDaysAgo = function(){
   return Date.now() - thirtyOneDaysInSeconds;
 }
 
-const getLastFiveFromTimestamp = function(){
-  return Date.now().toString().substr(-5);
+const getHumanReadableOneMonthFromNow = function(){
+  return (new Date((new Date()).getTime() + thirtyOneDaysInSeconds));
 }
 
-const getHumanReadableOneMonthFromNow = function(){
-  return (new Date((new Date()).getTime() + oneMonth));
+const getLastFiveFromTimestamp = function(){
+  return Date.now().toString().substr(-5);
 }
 
 exports.getTimestampFromThirtyOneDaysAgo = getTimestampFromThirtyOneDaysAgo;
