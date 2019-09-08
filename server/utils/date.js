168a5525b0bc55359764c8853654668ca1d27cc8
Joe Podwys <joseph@podwys.com>
2019-09-08 14:24:13 -0600

    Fix cron delete task (#68)
    * Make cron task for entry deletion do what it's supposed to do.
    
    * More cleanup
    
    * Fixing two typos. One is very important.
    
    * Change a function name.
    
    * Clarification
    
    * Whoops this is for a cookie expiration
    
    * Simplification

--- server/utils/date.js
+++ server/utils/date.js
@@ -1,5 +1,17 @@
+const thirtyOneDaysInSeconds = 31 * 24 * 60 * 60;
+
+const getTimestampFromThirtyOneDaysAgo = function(){
+  return Date.now() - thirtyOneDaysInSeconds;
+}
+
+const getHumanReadableOneMonthFromNow = function(){
+  return (new Date((new Date()).getTime() + thirtyOneDaysInSeconds));
+}
+
 const getLastFiveFromTimestamp = function(){
   return Date.now().toString().substr(-5);
 }
 
-exports.getLastFiveFromTimestamp =getLastFiveFromTimestamp;
+exports.getTimestampFromThirtyOneDaysAgo = getTimestampFromThirtyOneDaysAgo;
+exports.getLastFiveFromTimestamp = getLastFiveFromTimestamp;
+exports.getHumanReadableOneMonthFromNow = getHumanReadableOneMonthFromNow;
