fd77cc4a13a1059c237ea8786b5c0520f8d31120
Joe Podwys <joe@podwys.com>
2019-09-08 23:53:20 -0600

    Revert "Fix cron delete task (#68)" (#72)
    This reverts commit 168a5525b0bc55359764c8853654668ca1d27cc8.

--- server/utils/date.js
+++ server/utils/date.js
@@ -1,17 +1,5 @@
-const thirtyOneDaysInSeconds = 31 * 24 * 60 * 60;
-
-const getTimestampFromThirtyOneDaysAgo = function(){
-  return Date.now() - thirtyOneDaysInSeconds;
-}
-
-const getHumanReadableOneMonthFromNow = function(){
-  return (new Date((new Date()).getTime() + thirtyOneDaysInSeconds));
-}
-
 const getLastFiveFromTimestamp = function(){
   return Date.now().toString().substr(-5);
 }
 
-exports.getTimestampFromThirtyOneDaysAgo = getTimestampFromThirtyOneDaysAgo;
-exports.getLastFiveFromTimestamp = getLastFiveFromTimestamp;
-exports.getHumanReadableOneMonthFromNow = getHumanReadableOneMonthFromNow;
+exports.getLastFiveFromTimestamp =getLastFiveFromTimestamp;
