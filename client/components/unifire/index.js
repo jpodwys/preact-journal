ff441c1d8182eddee025eece114cfabb793dd7e5
Joseph Podwys <joseph@podwys.com>
2019-10-21 00:40:24 -0600

    Simplify unifire

--- client/components/unifire/index.js
+++ client/components/unifire/index.js
@@ -1,25 +1,12 @@
 import { h, Component, cloneElement } from 'preact';
 
+let EL;
 let STATE;
-const NAME = 'UNIFIRE';
+let ACTIONS;
 
-function listen (el, actions) {
-  document.addEventListener(NAME, e => {
-    // Array destructuring adds .19kb to the bundle.
-    // Doing it the less attractive way for now.
-    const d = e.detail;
-    // This line needs to be here due to how unifre and unit
-    // tests currently work.
-    if(!actions[d[0]]) return;
-    actions[d[0]](el, d[1], d[2]);
-  });
-};
-
-export function fire (name, detail) {
-  return (e) => {
-    const event = new CustomEvent(NAME, { detail: [ name, detail, e ] });
-    document.dispatchEvent(event);
-  }
+export function fire (name, payload) {
+  // if(!ACTIONS[name]) return;
+  return (e) => ACTIONS[name](EL, payload, e);
 };
 
 export class Provider extends Component {
@@ -27,9 +14,10 @@ export class Provider extends Component {
   // The Proxy handles computed properties and local persistence.
   constructor(props) {
     super(props);
+    EL = this;
     STATE = this.props.state;
+    ACTIONS = this.props.actions;
     this.child = props.children[0];
-    listen(this, this.props.actions);
     this.state = STATE;
   }
 
