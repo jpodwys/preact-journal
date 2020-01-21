333fa819a99f916a8434bc84a06566fbd40f48d7
Joseph Podwys <joseph@podwys.com>
2020-01-21 02:16:05 -0700

    Cleanup a route definition

---
+++ server/middleware/jwt-parser.js
@@ -0,0 +1,13 @@
+const jwtMW = require('express-jwt'),
+  AES = require('../utils/aes');
+
+export default jwtMW({
+  secret: process.env.JWT_KEY,
+  credentialsRequired: false,
+  getToken: (req) => {
+    if(req.cookies && req.cookies.auth_token){
+      return AES.decrypt(req.cookies.auth_token);
+    }
+    return null;
+  }
+});
