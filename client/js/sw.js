6a0c87bb17cc84cd4bdcdb7f73c42251f28d6a89
Joseph Podwys <joseph@podwys.com>
2021-03-07 20:22:09 -0700

    Stop inlinig JS

--- client/js/sw.js
+++ client/js/sw.js
@@ -2,7 +2,7 @@ const CACHE = 'preact-journal';
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(cache => {
-    cache.addAll(['/', '/manifest.json', '/version', '/favicon.ico', '/icon-144.png', '/icon-512.png']);
+    cache.addAll(['/', '/bundle.js', '/manifest.json', '/version', '/favicon.ico', '/icon-144.png', '/icon-512.png']);
   }));
 });
 
