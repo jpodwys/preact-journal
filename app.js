83a8c6b1caf3cf1dd2757fa6c4c5b4e862ac2205
Joe Podwys <joseph@podwys.com>
2023-03-01 08:18:08 -0700

    Trying out Heroku-22 (#122)
    * Trying out Heroku-22
    
    * Trying node 16
    
    * Trying to get the build running
    
    * Trying to get the build running
    
    * Trying to get the build running
    
    * shrink-ray-current -> compression :(
    
    * Remove the ability to create an account

--- app.js
+++ app.js
@@ -7,15 +7,20 @@ var express = require('express'),
   forceSsl = require('force-ssl-heroku'),
   jwtPrser = require('./server/middleware/jwt-parser'),
   cron = require('./server/cron'),
-  shrinkRay = require('shrink-ray-current');
+  // Can't use shrinkRay any more because it relies on
+  // a brotli library that uses an old version of python
+  // which gyp and therefore heroku don't support.
+  // This means the app's download size is significantly larger.
+  // But this is the price I have to pay for now to get this deployable again.
+  // shrinkRay = require('shrink-ray-current');
+  compression = require('compression'),
   PORT = process.env.PORT || 3000;
-
-
 
 app.disable('x-powered-by');
 app.use(forceSsl);
 app.use(strictTransportSecurity);
-app.use(shrinkRay({ threshold: '1.4kb' }));
+// app.use(shrinkRay({ threshold: '1.4kb' }));
+app.use(compression({ threshold: '1.4kb' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
