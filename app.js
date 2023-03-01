8e22eaaf1604b96f05969efcf11e740f5788d05d
jpodwys <joseph@podwys.com>
2023-02-28 23:06:41 -0700

    Trying to get the build running

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ var express = require('express'),
   forceSsl = require('force-ssl-heroku'),
   jwtPrser = require('./server/middleware/jwt-parser'),
   cron = require('./server/cron'),
-  shrinkRay = require('shrink-ray-current');
+  // shrinkRay = require('shrink-ray-current');
   PORT = process.env.PORT || 3000;
 
 app.disable('x-powered-by');
 app.use(forceSsl);
 app.use(strictTransportSecurity);
-app.use(shrinkRay({ threshold: '1.4kb' }));
+// app.use(shrinkRay({ threshold: '1.4kb' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
