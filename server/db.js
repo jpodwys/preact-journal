3ceed44263ed1d40aec79b40b23cafe5286a5f06
jpodwys <joseph@podwys.com>
2024-11-06 12:24:04 -0700

    Change DB providers

--- server/db.js
+++ server/db.js
@@ -1,22 +1,21 @@
 var isDevMode = (process.env.NODE_ENV === 'development');
+// var fs = require('fs');
+const awsCaBundle = require('aws-ssl-profiles');
 
 module.exports = function(Sequelize){
-  var db = new Sequelize(process.env.DB_URL, {
+  // var globalBundle = fs.readFileSync('./ssl/global-bundle.pem');
+  var db = new Sequelize(process.env.JAWSDB_URL, {
     dialectOptions: {
-      // debug: isDevMode,
       ssl: {
-        // Run this command for each SSL ENV
-        // heroku config:set SQL_SSL_CA="$(cat ca.pem)" -a riot-demo
-        // Run this to pull/push ENVs form heroku to .env or back
-        // heroku config:pull -a riot-demo
-        // heroku config:push -a riot-demo
-        ca: process.env.SQL_SSL_CA,
-        cert: process.env.SQL_SSL_CERT,
-        key: process.env.SQL_SSL_KEY
-      }
+        // ca: process.env.SQL_SSL_CA,
+        // cert: process.env.SQL_SSL_CERT,
+        // key: process.env.SQL_SSL_KEY,
+        rejectUnauthorized: true,
+        verifyIdentity: true,
+        ca: awsCaBundle.ca,
+      },
     },
     omitNull: true,
-    // logging: isDevMode ? console.log : false,
     benchmark: isDevMode
   });
   return db;
