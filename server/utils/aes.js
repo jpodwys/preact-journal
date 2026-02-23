08a5d41fcd4eaa096fd1ea9213bf0236e037de24
Joe Podwys <joepodwys@mac.lan>
2026-02-22 20:29:55 -0700

    Fix insecure AES encryption by migrating to createCipheriv with random IVs

--- server/utils/aes.js
+++ server/utils/aes.js
@@ -1,17 +1,33 @@
 var crypto = require('crypto'),
   algorithm = 'aes-256-ctr',
-  key = process.env.AES_KEY;
+  IV_LENGTH = 16;
+
+function getKey() {
+  var raw = process.env.AES_KEY;
+  return crypto.createHash('sha256').update(raw).digest();
+}
 
 exports.encrypt = function(text){
-  var cipher = crypto.createCipher(algorithm, key);
+  var iv = crypto.randomBytes(IV_LENGTH);
+  var cipher = crypto.createCipheriv(algorithm, getKey(), iv);
   var crypted = cipher.update(text, 'utf8', 'hex');
   crypted += cipher.final('hex');
-  return crypted;
+  return iv.toString('hex') + ':' + crypted;
 }
- 
+
 exports.decrypt = function(text){
-  var decipher = crypto.createDecipher(algorithm, key);
-  var dec = decipher.update(text, 'hex', 'utf8');
+  // Support legacy format (no IV prefix) for existing cookies
+  if(text.indexOf(':') === -1){
+    var legacyCipher = crypto.createDecipher(algorithm, process.env.AES_KEY);
+    var legacyDec = legacyCipher.update(text, 'hex', 'utf8');
+    legacyDec += legacyCipher.final('utf8');
+    return legacyDec;
+  }
+  var parts = text.split(':');
+  var iv = Buffer.from(parts[0], 'hex');
+  var encrypted = parts[1];
+  var decipher = crypto.createDecipheriv(algorithm, getKey(), iv);
+  var dec = decipher.update(encrypted, 'hex', 'utf8');
   dec += decipher.final('utf8');
   return dec;
 }
